f3ffd7039868bb36f1b75f3aa5a51b039c45e008
Max <cerbereon@gmail.com>
2018-03-21 16:30:45 +0300

    Currently on Chapter 5

---
+++ project1/venv/chapter 5/5.7_favorite_fruit.py
@@ -0,0 +1,18 @@
+'''Make a list of your favorite fruits, and then write a series of
+independent if statements that check for certain fruits in your list.
+    • Make a list of your three favorite fruits and call it favorite_fruits.
+    • Write five if statements. Each should check whether a certain kind of
+    fruit is in your list. If the fruit is in your list, the if block should
+    print a statement, such as You really like bananas!'''
+
+favorite_fruits = ['apple', 'pear', 'pineapple', 'kiwi']
+if 'apple' in favorite_fruits:
+    print('You really like apples!')
+if 'kiwi' in favorite_fruits:
+    print('You really like kiwis!')
+if 'banana' in favorite_fruits:
+    print('You really like bannas!')
+if 'pear' in favorite_fruits:
+    print('You really like pears!')
+if 'watermelon' in favorite_fruits:
+    print('You really like watemelons!')
--- project1/venv/chapter 5/toppings.py
+++ project1/venv/chapter 5/toppings.py
@@ -1,4 +1,52 @@
 requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+    print("Hold the anchovies!\n")
 
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!\n")
+
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!\n")
+
+
+requested_toppings = ['mushrooms', 'extra cheese', 'green peppers']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers rigth now.")
+    else:
+        print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!\n")
+
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!\n")
+else:
+    print("Are you sure, you want a plain pizza?")
