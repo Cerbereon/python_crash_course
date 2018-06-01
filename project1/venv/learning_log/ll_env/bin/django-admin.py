bf30c061ec75ec7360ddeecddd5bcde5f7c51de7
Max Korenev <cerbereon@gmail.com>
2018-06-01 14:28:08 +0300

    Started Project3

---
+++ project1/venv/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cerbereon/Programming/Projects/python_crash_course/project1/venv/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
