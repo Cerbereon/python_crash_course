8b2e8be82010432c9511c6f9c18aaee309d2a928
Max Korenev <cerbereon@gmail.com>
2018-06-04 08:35:01 +0300

    Started Blog Project. Created a template

---
+++ project1/venv/projects/blog/blog_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cerbereon/Programming/Projects/python_crash_course/project1/venv/projects/blog/blog_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
